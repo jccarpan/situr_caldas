78125367d3d92a017c283b910435aa47e2b7b736
jccarpan <chejoma44@hotmail.com>
2014-05-20 01:26:37 -0500

    otro commit

---
+++ imagenes/menu.php
@@ -0,0 +1,329 @@
+
+<!-- Demo navbar -->
+    <div class="navbar yamm navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" data-toggle="collapse" data-target="#navbar-collapse-1" class="navbar-toggle"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a href="#" class="navbar-brand">SITUR CALDAS</a>
+        </div>
+        <div id="navbar-collapse-1" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <!-- Classic list -->
+            <li class="dropdown"><a href="#" data-toggle="dropdown" class="dropdown-toggle">Destinos<b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li>
+                  <!-- Content container to add padding -->
+                  <div class="yamm-content">
+                    <div class="row">
+                      <ul class="col-sm-4 list-unstyled">
+                        <li class="divisor"></li>
+                        <li><p><strong>Alto Occidente</strong></p></li>
+                        <li class="divisor"></li>
+                        <li><a class="enlace" href="#">Filadelfia</a></li>
+                        <li><a class="enlace" href="#">La Merced</a></li>
+                        <li><a class="enlace" href="#">Marmato</a></li>
+                        <li><a class="enlace" href="#">Riosucio</a></li>
+                        <li><a class="enlace" href="#">Supia</a></li>
+                      </ul>
+                      <ul class="col-sm-4 list-unstyled">
+                        <li class="divisor"></li>
+                        <li><p><strong>Alto Oriente</strong></p></li>
+                        <li class="divisor"></li>
+                        <li><a class="enlace" href="#"> Manzanares</a></li>
+                        <li><a class="enlace" href="#"> Marquetalia</a></li>
+                        <li><a class="enlace" href="#"> Marulanda</a></li>
+                        <li><a class="enlace" href="#"> Pensilvania</a></li>
+                      </ul>
+                      <ul class="col-sm-4 list-unstyled">
+                        <li class="divisor"></li>
+                        <li><p><strong>Bajo Occidente</strong></p></li>
+                        <li class="divisor"></li>
+                        <li><a class="enlace" href="#">Anserma</a></li>
+                        <li><a class="enlace" href="#">Belalcazar</a></li>
+                        <li><a class="enlace" href="#">Risaralda</a></li>
+                        <li><a class="enlace" href="#">San Jose</a></li>
+                        <li><a class="enlace" href="#">Viterbo</a></li>
+                      </ul>
+                      <ul class="col-sm-4 list-unstyled">
+                        <li class="divisor"></li>
+                        <li><p><strong>Centro Sur</strong></p></li>
+                        <li class="divisor"></li>
+                        <li><a class="enlace" href="#">Chinchica</a></li>
+                        <li><a class="enlace" href="#">Manizales</a></li>
+                        <li><a class="enlace" href="#">Neira</a></li>
+                        <li><a class="enlace" href="#">Palestina</a></li>
+                        <li><a class="enlace" href="#">Villamaria</a></li>
+                      </ul>
+                      <ul class="col-sm-4 list-unstyled">
+                        <li class="divisor"></li>
+                        <li><p><strong>Magdalena Caldense</strong></p></li>
+                        <li class="divisor"></li>
+                        <li><a class="enlace" href="#">La Dorada</a></li>
+                        <li><a class="enlace" href="#">Norcasia</a></li>
+                        <li><a class="enlace" href="#">Samana</a></li>
+                        <li><a class="enlace" href="#">Victoria</a></li>
+                      </ul>
+                      <ul class="col-sm-4 list-unstyled">
+                        <li class="divisor"></li>
+                        <li><p><strong>Norte</strong></p></li>
+                        <li class="divisor"></li>
+                        <li><a class="enlace" href="#">Aguadas</a></li>
+                        <li><a class="enlace" href="#">Aranzazu</a></li>
+                        <li><a class="enlace" href="#">Pacora</a></li>
+                        <li><a class="enlace" href="#">Salamina</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </li>
+
+<!-- Atractivos Turisticos -->
+            <li class="dropdown"><a href="#" data-toggle="dropdown" class="dropdown-toggle"> Servicios Turisticos <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li>
+                  <!-- Content container to add padding -->
+                  <div class="yamm-content">
+                    <div class="row">
+                      <ul class="col-sm-4 list-unstyled">
+                        <li class="divisor"></li>
+                        <li><p><strong> Bancarios</strong></p></li>
+                        <li class="divisor"></li>
+                        <li><a class="enlace" href="#"> Bancos</a></li>
+                        <li><a class="enlace" href="#"> Casas de Cambio</a></li>
+                      </ul>
+                      <ul class="col-sm-4 list-unstyled">
+                        <li class="divisor"></li>
+                        <li><p><strong> Entretenimiento y Diversion</strong></p></li>
+                        <li class="divisor"></li>
+                        <li><a class="enlace" href="#"> Centros Comerciales</a></li>
+                        <li><a class="enlace" href="#"> Centros Deportivos</a></li>
+                        <li><a class="enlace" href="#"> Centros Vacacionales</a></li>
+                      </ul>
+                      <ul class="col-sm-4 list-unstyled">
+                        <li class="divisor"></li>
+                        <li><p><strong> Informacion y Guias</strong></p></li>
+                        <li class="divisor"></li>
+                        <li><a class="enlace" href="#"> Oficinas de Turismo</a></li>
+                        <li><a class="enlace" href="#"> Puntos de Informacion</a></li>
+                        <li><a class="enlace" href="#"> Agencias de Viaje</a></li>
+                      </ul>
+                      <ul class="col-sm-4 list-unstyled">
+                        <li class="divisor"></li>
+                        <li><p><strong> Hoteleria y Gastronomia</strong></p></li>
+                        <li class="divisor"></li>
+                        <li><a class="enlace" href="#"> Hoteles</a></li>
+                        <li><a class="enlace" href="#"> Restaurantes</a></li>
+                      </ul>
+                      <ul class="col-sm-4 list-unstyled">
+                        <li class="divisor"></li>
+                        <li><p><strong> Transporte</strong></p></li>
+                        <li class="divisor"></li>
+                        <li><a class="enlace" href="#"> Empresas de Transporte</a></li>
+                        <li><a class="enlace" href="#"> Terminales de transporte</a></li>
+                        <li><a class="enlace" href="#"> Estaciones de Servicio</a></li>
+                      </ul>
+                      <ul class="col-sm-4 list-unstyled">
+                        <li class="divisor"></li>
+                        <li><p><strong> Salud y Seguridad</strong></p></li>
+                        <li class="divisor"></li>
+                        <li><a class="enlace" href="#"> Hospitales</a></li>
+                        <li><a class="enlace" href="#"> Droguerias</a></li>
+                        <li><a class="enlace" href="#"> Estaciones de Policia</a></li>
+                        <li><a class="enlace" href="#"> Bomberos </a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </li>
+
+
+            <!-- Atractivos Turisticos -->
+
+            <li class="dropdown"><a href="#" data-toggle="dropdown" class="dropdown-toggle"> Atractivos Turisticos <b class="caret"></b></a>
+              <ul role="menu" class="dropdown-menu">
+                <li><a tabindex="-1" class="enlace" href="#"> Arquitectonicos </a></li>
+                <li><a tabindex="-1" class="enlace" href="#"> Bibliotecas </a></li>
+                <li><a tabindex="-1" class="enlace" href="#"> Museos </a></li>
+                <li><a tabindex="-1" class="enlace" href="#"> Naturales </a></li>
+                <li><a tabindex="-1" class="enlace" href="#"> Plazas y Parques </a></li>
+                <li><a tabindex="-1" class="enlace" href="#"> Teatros </a></li>
+              </ul>
+            </li>
+
+
+            <!-- Observatorio de Turismo -->
+            <li class="dropdown"><a href="#" data-toggle="dropdown" class="dropdown-toggle"> Observatorio de Turismo <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li>
+                  <!-- Content container to add padding -->
+                  <div class="yamm-content">
+                    <div class="row">
+                      <ul class="col-sm-8 list-unstyled">
+                        <li class="divisor"></li>
+                        <li><p><strong> Instituciones</strong></p></li>
+                        <li class="divisor"></li>
+                        <li><a class="enlace" href="#"> Cámara de Comercio de Manizales </a></li>
+                        <li><a class="enlace" href="#"> Comité de Cafeteros </a></li>
+                        <li><a class="enlace" href="#"> Confamiliares</a></li>
+                        <li><a class="enlace" href="#"> Corporación para el Desarrollo de Caldas</a></li>
+                        <li><a class="enlace" href="#"> Cotelco </a></li>
+                        <li><a class="enlace" href="#"> Fenalco</a></li>
+                        <li><a class="enlace" href="#"> Gobernación de Caldas </a></li>
+                        <li><a class="enlace" href="#"> INFI Manizales </a></li>
+                        <li><a class="enlace" href="#"> Instituto de Cultura y Turismo de Manizales</a></li>
+                        <li><a class="enlace" href="#"> Mesa de Turismo de Caldas</a></li>
+                        <li><a class="enlace" href="#"> Secretaria de Competitividad y Fomento Empresarial</a></li>
+                        <li><a class="enlace" href="#"> Secretaria de Cultura del Departamento</a></li>
+                        <li><a class="enlace" href="#"> Sena</a></li>
+                      </ul>
+                      <ul class="col-sm-4 list-unstyled">
+                        <li class="divisor"></li>
+                        <li><p><strong> Indicadores</strong></p></li>
+                        <li class="divisor"></li>
+                        <li><a class="enlace" href="#"> Indicadores Trimestrales</a></li>
+                        <li><a class="enlace" href="#"> Indicadores Semestrales</a></li>
+                        <li><a class="enlace" href="#"> Indicadores Anuales</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </li>
+
+
+
+
+            <!-- Accordion demo -->
+            <!-- <li class="dropdown"><a href="#" data-toggle="dropdown" class="dropdown-toggle">Accordion<b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li>
+                  <div class="yamm-content">
+                    <div class="row">
+                      <div id="accordion" class="panel-group">
+                        <div class="panel panel-default">
+                          <div class="panel-heading">
+                            <h4 class="panel-title"><a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">Collapsible Group Item #1</a></h4>
+                          </div>
+                          <div id="collapseOne" class="panel-collapse collapse in">
+                            <div class="panel-body">Ut consectetur ullamcorper purus a rutrum. Etiam dui nisi, hendrerit feugiat scelerisque et, cursus eu magna. </div>
+                          </div>
+                        </div>
+                        <div class="panel panel-default">
+                          <div class="panel-heading">
+                            <h4 class="panel-title"><a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">Collapsible Group Item #2</a></h4>
+                          </div>
+                          <div id="collapseTwo" class="panel-collapse collapse">
+                            <div class="panel-body">Nullam pretium fermentum sapien ut convallis. Suspendisse vehicula, magna non tristique tincidunt, massa nisi luctus tellus, vel laoreet sem lectus ut nibh. </div>
+                          </div>
+                        </div>
+                        <div class="panel panel-default">
+                          <div class="panel-heading">
+                            <h4 class="panel-title"><a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">Collapsible Group Item #3</a></h4>
+                          </div>
+                          <div id="collapseThree" class="panel-collapse collapse">
+                            <div class="panel-body">Praesent leo quam, faucibus at facilisis id, rhoncus sit amet metus. Sed vitae ipsum non nibh mattis congue eget id augue. </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </li> -->
+
+
+            <!-- Classic dropdown -->
+            <!-- <li class="dropdown"><a href="#" data-toggle="dropdown" class="dropdown-toggle">Classic<b class="caret"></b></a>
+              <ul role="menu" class="dropdown-menu">
+                <li><a tabindex="-1" href="#"> Action </a></li>
+                <li><a tabindex="-1" href="#"> Another action </a></li>
+                <li><a tabindex="-1" href="#"> Something else here </a></li>
+                <li class="divider"></li>
+                <li><a tabindex="-1" href="#"> Separated link </a></li>
+              </ul>
+            </li> -->
+            
+            <!-- Pictures -->
+            <li class="dropdown yamm-fw"><a href="#" data-toggle="dropdown" class="dropdown-toggle">Destacamos<b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li>
+                  <div class="yamm-content">
+                    <div class="row">
+                      <div class="col-sm-3">
+                          <div class="thumbnail"><img alt="260x130" src="imagenes/cultura_cafetera_1.png">
+                            <div class="caption">
+                              <h3>Cultura cafetera</h3>
+                              <p>Mazagran doppio half and half aftertaste organic, rich doppio</p>
+                              <p><a href="#" class="btn btn-primary">Mas...</a> <!-- <a href="#" class="btn btn-default">Action</a> --></p>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-sm-3">
+                          <div class="thumbnail"><img alt="260x130" src="imagenes/salamina_1.jpg">
+                            <div class="caption">
+                              <h3>Salamina: ciudad luz</h3>
+                              <p>Mazagran doppio half and half aftertaste organic, rich doppio</p>
+                              <p><a href="#" class="btn btn-primary">Mas...</a> <!-- <a href="#" class="btn btn-default">Action</a> --></p>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-sm-3">
+                          <div class="thumbnail"><img alt="260x130" src="imagenes/salamina_2.jpg">
+                            <div class="caption">
+                              <h3>Fiestas religiosas</h3>
+                              <p>Mazagran doppio half and half aftertaste organic, rich doppio</p>
+                              <p><a href="#" class="btn btn-primary">Mas...</a> <!-- <a href="#" class="btn btn-default">Action</a> --></p>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-sm-3">
+                          <div class="thumbnail"><img alt="260x130" src="imagenes/nevado_ruiz_1.png">
+                            <div class="caption">
+                              <h3>Nevado del ru</h3>
+                              <p>Mazagran doppio half and half aftertaste organic, rich doppio</p>
+                              <p><a href="#" class="btn btn-primary">Mas...</a> <!-- <a href="#" class="btn btn-default">Action</a> --></p>
+                            </div>
+                          </div>
+                        </div>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </li>
+          </ul>
+
+		<!-- formulario derecha -->
+
+		<ul class="nav navbar-nav navbar-right">
+              <!-- Forms -->
+              <li class="dropdown"><a href="#" data-toggle="dropdown" class="dropdown-toggle">Log in<b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li>
+                    <div class="yamm-content">
+                      <form action="send.php">
+                        <div class="form-group">
+                          <input id="inputName" type="text" placeholder="Usuario" class="form-control">
+                        </div>
+                        <div class="form-group">
+                          <input id="inputEmail" type="password" placeholder="Contraseña" class="form-control">
+                        </div>
+                        <div class="form-group">
+                          <a href=""><p>¿Olvidaste tu contraseña?</p></a>
+                        </div>
+                        <div class="form-group">
+                          <button type="submit" class="btn btn-success">Enviar</button>
+                          <button type="submit" class="btn btn-primary">Registrate</button>
+                        </div>
+                      </form>
+                    </div>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+
+		<!-- fin formulario derecha -->
+
+        </div>
+      </div>
+    </div>
